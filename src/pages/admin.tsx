1d85c50103e5690635b9a23111b3e7fbed6aead4
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-09-09 19:41:39 -0300

    Use expect error instead of ignore

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,8 +1,8 @@
 import dynamic from 'next/dynamic';
 
-// @ts-ignore
+// @ts-expect-error
 const CMS = dynamic(() => import('decap-cms-app').then((cms) => {
-    // @ts-ignore
+    // @ts-expect-error
     cms.init()
 }), {ssr: false});
 
