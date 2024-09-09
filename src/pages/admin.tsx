669e0266d8941cbf7858b84782214b3e8c110d7f
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-09-09 19:43:10 -0300

    Add description

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,8 +1,8 @@
 import dynamic from 'next/dynamic';
 
-// @ts-expect-error
+// @ts-expect-error If I don't add a description, next will yell at me 
 const CMS = dynamic(() => import('decap-cms-app').then((cms) => {
-    // @ts-expect-error
+    // @ts-expect-error If I don't add a description, next will yell at me
     cms.init()
 }), {ssr: false});
 
