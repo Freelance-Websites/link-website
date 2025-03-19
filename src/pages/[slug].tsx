48e20a6a40afe282c087d18468c03a85721e83e0
Juan Martin Garcia <hi@juanmartingarcia.com>
2025-03-19 14:49:07 -0300

    Linting

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import dynamic from 'next/dynamic';
 
 import Main from '@/components/Main';
 import Slider, { SliderProps } from '@/components/home/Slider';
