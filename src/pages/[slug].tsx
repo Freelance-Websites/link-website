0651eac0a3e5ecaf5a9f7996bb14c16bf8db39d9
Juan Martin Garcia <hi@juanmartingarcia.com>
2025-03-27 17:46:14 -0300

    Prop fixes

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -20,7 +20,7 @@ export default function Page() {
   
   return (
     <MainPage
-      title={attributes?.title}
+      title={attributes?.title || ''}
       sections={attributes?.sections || []}
     />
   );
