cddee908f1d23c27c1352fedce715cbd8abc4521
Juan Martin Garcia <hi@juanmartingarcia.com>
2025-03-28 11:53:16 -0300

    Add back redirec

--- next.config.mjs
+++ next.config.mjs
@@ -13,15 +13,15 @@ const nextConfig = {
     return cfg;
   },
 
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/home',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
