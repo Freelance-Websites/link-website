d65568842942e364675f363d4fc9bb1faaa03b9a
Juan Martín García <juanmartingarcia@juanmartingarcia.local>
2025-05-05 18:08:17 -0300

    Add event to redirect after login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,8 @@ export default function Page() {
   useEffect(() => {
     // @ts-expect-error: Suppressing TypeScript error for netlifyIdentity as its type definitions are not available
     netlifyIdentity.open();
+    // @ts-expect-error: Suppressing TypeScript error for netlifyIdentity as its type definitions are not available
+    netlifyIdentity.on('login', window.location.replace('/admin'));
   }, []);
   
   return (
@@ -12,9 +14,6 @@ export default function Page() {
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async />
       </Head>
-      {/* <main>
-        <p>Login</p>
-      </main> */}
     </>
   );
 }
