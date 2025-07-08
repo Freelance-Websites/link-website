a361a181b4f7d0541bf9717c7ae4085f98081c61
Juan Martin Garcia <hi@juanmartingarcia.com>
2025-07-08 13:13:50 -0300

    Replace logo

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -103,7 +103,7 @@ export default function MainPage({
           onClick={() => closeAnimation()}
         >
           <img
-            src="/images/logo.png"
+            src="/images/logo.svg"
             alt="Link Logo"
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-32 h-32 lg:w-44 lg:h-44"
           />
